f9ed47683922dd450612327218688734de32cbdd
fooofei <aihujianfei@gmail.com>
2018-08-30 22:57:52 +0800

    func

--- func_test.go
+++ func_test.go
@@ -33,3 +33,43 @@ func ExampleClosureFunc(){
     //9 45  -90
 }
 
+func change(v func(int)int) int{
+    return v(11)
+}
+
+// closure func as param will be reference
+func ExampleClosureFuncParam(){
+    var a = addr()
+    fmt.Printf("a(1)=%v\n",a(1))
+    fmt.Printf("a(2)=%v\n",a(2))
+    fmt.Printf("a(0)=%v\n",a(0))
+    fmt.Printf("change(a)=%v\n", change(a))
+    fmt.Printf("a(0)=%v\n", a(0))
+    //output:
+    //a(1)=1
+    //a(2)=3
+    //a(0)=3
+    //change(a)=14
+    //a(0)=14
+}
+
+func ExampleClosureFuncCopy(){
+    var a = addr()
+    fmt.Printf("a(1)=%v\n",a(1))
+    fmt.Printf("a(2)=%v\n",a(2))
+    fmt.Printf("a(0)=%v\n",a(0))
+
+    var b = a
+    fmt.Printf("b(0)=%v\n",b(0))
+    fmt.Printf("b(1)=%v\n",b(1))
+    fmt.Printf("a(0)=%v\n",a(0))
+    fmt.Printf("b(0)=%v\n",b(0))
+    //output:
+    //a(1)=1
+    //a(2)=3
+    //a(0)=3
+    //b(0)=3
+    //b(1)=4
+    //a(0)=4
+    //b(0)=4
+}
