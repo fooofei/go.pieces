bec013631aa4be05d9677a32a0e2bf15351c0d06
fooofei <aihujianfei@gmail.com>
2018-10-08 23:24:35 +0800

    a version

--- socket/uds-rx/uds-rx.go
+++ socket/uds-rx/uds-rx.go
@@ -4,11 +4,24 @@ import (
     "fmt"
     "net"
     "os"
+    "time"
 )
+
+type hitStat struct{
+    hitTime time.Time
+    rx uint64
+    rxOk uint64
+    rxFail uint64
+}
+func subTime(startTime time.Time, n time.Time) string{
+    d := n.Sub(startTime)
+    return fmt.Sprintf("%v %v(s)",n.Format("2006-01-02 15:04:05"),
+        d.Seconds())
+}
 
 func main(){
     sockType := "unixgram"
-    localAddr,_ := net.ResolveUnixAddr(sockType, fmt.Sprintf("/tmp/dpdk-rx.sock"))
+    localAddr,_ := net.ResolveUnixAddr(sockType, fmt.Sprintf("/tmp/dpdk-tx-1-0.sock"))
     os.Remove(localAddr.String())
     conn,err :=net.ListenUnixgram(sockType, localAddr)
     //conn0,err :=net.Listen("unixgram", localAddrS)
@@ -17,20 +30,43 @@ func main(){
         panic(err)
     }
     defer os.Remove(localAddr.String())
-    rxBuf := make([]byte, 0, 1024*2)
-    cnt := 0
-
+    rxBuf := make([]byte, 1024*2)
+    var hit hitStat
+    var stat hitStat
+    stat.hitTime=time.Now()
+    hit.hitTime = stat.hitTime
     for{
-        rx0, remoteAddr, err := conn.ReadFromUnix(rxBuf)
-        cnt ++
-        rxBufS := fmt.Sprintf("%s", rxBuf)
-        fmt.Printf("cnt=%v %v: [len=%v return=%v]%v, err=%v\n",
-            cnt, remoteAddr, len(rxBuf),rx0, rxBufS,err)
+        nbRx, remoteAddr, err := conn.ReadFromUnix(rxBuf)
+        stat.rx ++
+        if nbRx>0{
+            stat.rxOk ++
+        }else{
+            stat.rxFail ++
+        }
+        //rxBufS := fmt.Sprintf("%s", rxBuf)
+        //fmt.Printf("cnt=%v %v: [len=%v return=%v]%v, err=%v\n",
+        //    cnt, remoteAddr, len(rxBuf),rx0, rxBufS,err)
+        //time.Sleep(time.Duration(time.Second))
+        _ = remoteAddr
+        _ = err
+        if stat.rx % 1000 ==0{
+            now:= time.Now()
+            d := now.Sub(hit.hitTime)
+            secs := uint64(d.Seconds())
+            if secs>0{
+                var p hitStat
+                p.rx = (stat.rx-hit.rx)/secs
+                p.rxOk = (stat.rxOk - hit.rxOk)/secs
+                p.rxFail = (stat.rxFail - hit.rxFail)/secs
+                hit = stat
+                hit.hitTime = now
+                fmt.Printf("%v %v(s) rx=%v rxok=%v rxfail=%v\n",subTime(stat.hitTime,now),secs,
+                    p.rx,p.rxOk,p.rxFail)
+            }
+
+        }
+
     }
-
-
-
-
 
     fmt.Printf("rx end\n")
 }
--- socket/uds-tx/uds-tx.go
+++ socket/uds-tx/uds-tx.go
@@ -8,33 +8,42 @@ import (
     "os"
     "os/signal"
     "path/filepath"
+    "sort"
     "strconv"
     "strings"
     "sync"
     "sync/atomic"
     "runtime"
-    //"time"
+    "time"
 )
 
 
 
 type routineStat  struct {
     tx * uint64
+    txOk * uint64
+    txFail * uint64
     id int
     remoteAddr string
     routineTx uint64
+    routineTxOk uint64
+    routineTxFail uint64
     stop bool
     stoped bool
     wg * sync.WaitGroup
 }
 
 func (stat * routineStat)String() string{
-    return fmt.Sprintf("id=%v remoteAddr=%v routineTx=%v stop=%v stoped=%v",
-        stat.id, stat.remoteAddr, stat.routineTx, stat.stop, stat.stoped)
+    return fmt.Sprintf("id=%v remoteAddr=%v routineTx=%v ok=%v fail=%v stop=%v stoped=%v",
+        stat.id, stat.remoteAddr, stat.routineTx,
+        stat.routineTxOk, stat.routineTxFail,
+        stat.stop, stat.stoped)
 }
 
 func (stat * routineStat)run(){
     defer stat.wg.Done()
+    stat.wg.Add(1)
+
     proto := "uds://"
     remoteAddrS := stat.remoteAddr
     remoteAddrS = strings.TrimPrefix(remoteAddrS,proto)
@@ -63,17 +72,26 @@ func (stat * routineStat)run(){
         txBuf.Reset()
         txBuf.WriteString(fmt.Sprintf("msg %v", atomic.LoadUint64(stat.tx)))
         txBytes := txBuf.Bytes()
-        fmt.Printf("tx len=%v\n", len(txBytes))
+        //fmt.Printf("tx len=%v\n", len(txBytes))
         rx0,err := conn.WriteToUnix(txBytes,remoteAddr)
         stat.routineTx ++
         atomic.AddUint64(stat.tx, 1)
+        if err != nil{
+            stat.routineTxFail ++
+            atomic.AddUint64(stat.txFail,1)
+        }else{
+         stat.routineTxOk ++
+         atomic.AddUint64(stat.txOk,1)
+        }
 
-        fmt.Printf("%v rx from %v %v len(rxBuf)=%v err=%v\n",stat, remoteAddr, rx0, len(txBuf.Bytes()), err)
+
+        _ = rx0
+        _ = err
+        //fmt.Printf("%v rx from %v %v len(rxBuf)=%v err=%v\n",stat, remoteAddr, rx0, len(txBuf.Bytes()), err)
         //time.Sleep(time.Duration(time.Second*3))
     }
-
-    fmt.Printf("%v exit\n", stat)
     stat.stoped=true
+    fmt.Printf("exit %v\n", stat)
 }
 
 type routineStats [] * routineStat
@@ -85,6 +103,42 @@ func (stat routineStats) Swap(i int, j int) {
 }
 func (stat routineStats) Less(i int, j int) bool {
     return stat[i].id < stat[j].id
+}
+func (stat routineStats) mapId() map[int]*routineStat{
+    r := make(map[int]*routineStat)
+    for _, s := range stat{
+       r[s.id]=s
+    }
+    return r
+}
+
+func mergeStat(oldStat routineStats, newStat routineStats) routineStats {
+    oldIdm := oldStat.mapId()
+    newIdm := newStat.mapId()
+
+    stat := make(routineStats,0)
+
+    // stop old
+    for _, o := range oldStat{
+        if _,ok := newIdm[o.id]; !ok{
+            fmt.Printf("sub %v\n", o)
+            o.stop=true
+            for !o.stoped{
+                time.Sleep(time.Duration(time.Second))
+            }
+        }
+    }
+    // start new
+    for _, n := range newStat{
+        if _,ok := oldIdm[n.id]; !ok {
+            fmt.Printf("add %v\n", n)
+            stat = append(stat,n)
+            go n.run()
+        }else{
+            stat = append(stat,oldIdm[n.id])
+        }
+    }
+    return stat
 }
 
 func oneTest(){
@@ -132,7 +186,8 @@ func Map(vs []string, f func(string) string) []string {
     return vsm
 }
 
-func getTasksFromFile() routineStats {
+func getTasksFromFile(tx * uint64, txOk * uint64, txFail * uint64,
+    group * sync.WaitGroup) routineStats {
 
     stat := make(routineStats, 0)
     curDir := getCurrentDir()
@@ -145,7 +200,7 @@ func getTasksFromFile() routineStats {
     defer file.Close()
 
     sc := bufio.NewScanner(file)
-    var set map[int] bool
+    set := make(map[int]bool)
     for sc.Scan(){
         line := sc.Text()
         if strings.HasPrefix(line, "# uds://"){
@@ -157,6 +212,10 @@ func getTasksFromFile() routineStats {
             if len(ss) ==2{
                 t := new(routineStat)
                 t.remoteAddr = ss[0]
+                t.tx = tx
+                t.wg = group
+                t.txOk = txOk
+                t.txFail = txFail
                 v,_ := strconv.ParseInt(ss[1],10,64)
                 t.id = int(v)
                 if _,ok := set[t.id]; !ok{
@@ -167,8 +226,14 @@ func getTasksFromFile() routineStats {
         }
 
     }
+    sort.Sort(stat)
     return stat
+}
 
+func subTime(startTime time.Time, n time.Time) string{
+    d := n.Sub(startTime)
+    return fmt.Sprintf("%v %v(s)",n.Format("2006-01-02 15:04:05"),
+        d.Seconds())
 }
 
 func main(){
@@ -177,12 +242,32 @@ func main(){
     signal.Notify(cSignal, os.Interrupt, os.Kill)
 
     var tx uint64
+    var txOk uint64
+    var txFail uint64
+    var wg sync.WaitGroup
 
+    txStat := make(routineStats,0)
+    startTime := time.Now()
 
-    txStat := getTasksFromFile()
-
-    for idx, stat := range txStat{
-        fmt.Printf("%v %v\n", idx, stat)
+    serveForever:
+    for{
+        newStat := getTasksFromFile(&tx,&txOk, &txFail, &wg)
+        txStat = mergeStat(txStat,newStat)
+        if len(txStat)==0{
+            fmt.Printf("- no tasks\n")
+        }
+        select {
+        case c:= <- cSignal:
+            fmt.Printf("got signal %v\n", c)
+            for _,s := range txStat{
+                s.stop=true
+            }
+            break serveForever
+        case <-time.After(time.Duration(time.Second*3)):
+        }
     }
-    fmt.Printf("main exit tx=%v\n", tx)
+
+    fmt.Printf("sync.WaitGroup wait \n")
+    wg.Wait()
+    fmt.Printf("main exit tx=%v take %v\n", tx, subTime(startTime,time.Now()))
 }
