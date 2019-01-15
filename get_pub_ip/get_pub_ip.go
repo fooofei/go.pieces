fb7c47f6d21b801d3f4d7d94a890281e16732a57
fooofei <aihujianfei@gmail.com>
2019-01-15 13:27:12 +0800

    more strong get ip

--- get_pub_ip/get_pub_ip.go
+++ get_pub_ip/get_pub_ip.go
@@ -3,7 +3,9 @@ package main
 import (
     "container/list"
     "context"
+    "encoding/json"
     "fmt"
+    "io"
     "io/ioutil"
     "log"
     "net/http"
@@ -15,21 +17,32 @@ import (
 )
 
 type ParallelHttpCtx struct {
-    ResultCh        chan string
+    ResultCh        chan *WorkItem
     AllResultDoneCh chan struct{}
     Results         *list.List
     Wg              sync.WaitGroup
     WaitCtx         context.Context
 }
+type IpGetter func(r io.Reader) (string, error)
+
+type WorkItem struct {
+    Uri      string
+    IpGetter IpGetter
+    Result   string
+}
 
 // do http.get with context.Context
 // context can be cancel()
-// not care about err, only push resp.body to chan
-func fetchIpRoutine(url string, ctx *ParallelHttpCtx) {
+// not care about err, only push result to chan
+func fetchIpRoutine(wk *WorkItem, ctx *ParallelHttpCtx) {
     defer ctx.Wg.Done()
+    defer func() {
+        // always push result
+        ctx.ResultCh <- wk
+    }()
     //
     clt := &http.Client{}
-    req, err := http.NewRequest(http.MethodGet, url, nil)
+    req, err := http.NewRequest(http.MethodGet, wk.Uri, nil)
     if err != nil {
         return
     }
@@ -42,12 +55,11 @@ func fetchIpRoutine(url string, ctx *ParallelHttpCtx) {
     if resp.StatusCode != 200 {
         return
     }
-    b, err := ioutil.ReadAll(resp.Body)
+    v, err := wk.IpGetter(resp.Body)
     if err != nil {
         return
     }
-    v := strings.TrimSpace(string(b))
-    ctx.ResultCh <- v
+    wk.Result = strings.TrimSpace(string(v))
 }
 
 // wait all sub routines result
@@ -78,14 +90,20 @@ func (p PairArray) Len() int               { return len(p) }
 func (p PairArray) Less(i int, j int) bool { return p[i].Value < p[j].Value }
 func (p PairArray) Swap(i int, j int)      { p[i], p[j] = p[j], p[i] }
 func getTop(ctx *ParallelHttpCtx) string {
+    const defaultr = "0.0.0.1"
     if ctx.Results.Len() <= 0 {
-        return ""
+        return defaultr
     }
     //
     rm := make(map[string]int, ctx.Results.Len())
     for e := ctx.Results.Front(); e != nil; e = e.Next() {
-        v := e.Value.(string)
-        rm[v] ++
+        v := e.Value.(*WorkItem)
+        if v.Result != "" {
+            rm[v.Result] ++
+        }
+    }
+    if len(rm) <= 0 {
+        return defaultr
     }
     ra := make(PairArray, len(rm))
     i := 0
@@ -97,48 +115,110 @@ func getTop(ctx *ParallelHttpCtx) string {
     return ra[0].Key
 }
 
-// 1 请求不必要返回 json 类型，省去我们要 decode json 的步骤
-// 2 并行发起http请求
-// 3 给定超时，有几个返回结果用几个返回结果
+// parse result for https://ip.nf/me.json
+func GetIpInJsonIpIp(r io.Reader) (string, error) {
+    b, err := ioutil.ReadAll(r)
+    if err != nil {
+        return "", err
+    }
+    m := make(map[string]interface{})
+    _ = json.Unmarshal(b, &m)
+    ipObj, ok := m["ip"].(map[string]interface{})
+    if ok {
+        ip, ok := ipObj["ip"].(string)
+        if ok {
+            return ip, nil
+        }
+    }
+    return "", fmt.Errorf("not found ip in %v", string(b))
+}
+
+// parse result for http://ip-api.com/json
+func GetIpInJsonQuery(r io.Reader) (string, error) {
+    b, err := ioutil.ReadAll(r)
+    if err != nil {
+        return "", err
+    }
+    m := make(map[string]interface{})
+    _ = json.Unmarshal(b, &m)
+    ip, ok := m["query"].(string)
+    if ok {
+        return ip, nil
+    }
+    return "", fmt.Errorf("not found ip in %v", string(b))
+}
+
+// parse result for https://wtfismyip.com/json
+func GetIpInJsonYourFuck(r io.Reader) (string, error) {
+    b, err := ioutil.ReadAll(r)
+    if err != nil {
+        return "", err
+    }
+    m := make(map[string]interface{})
+    _ = json.Unmarshal(b, &m)
+    ip, ok := m["YourFuckingIPAddress"].(string)
+    if ok {
+        return ip, nil
+    }
+    return "", fmt.Errorf("not found ip in %v", string(b))
+}
+func GetIpInPlainText(r io.Reader) (string, error) {
+    b, err := ioutil.ReadAll(r)
+    if err != nil {
+        return "", err
+    }
+    v := strings.TrimSpace(string(b))
+    return v, nil
+}
+
+// 1 并行发起http请求
+// 2 给定超时，有几个返回结果用几个返回结果
 //   且如果在超时时间内全部请求得到返回，这将会是更好的场面，
 //   我们就不必要一直死等超时，直接取用结果
-// 4 没有必要设置捕获 signal 信号，CTRL +C 可以在任意时刻退出，go 保证
+// 3 没有必要设置捕获 signal 信号，CTRL +C 可以在任意时刻退出，go 保证
 //   我们也没有要优雅退出的需求
 func main() {
     // no need to use https://api.ipify.org/?format=json
-    pubSrvs := [...]string{
-        "https://api.ipify.org",
-        "https://ip.seeip.org",
-        "https://ifconfig.me/ip",
-        "https://ifconfig.co/ip",
+    pubSrvs := &[...]WorkItem{
+        {Uri: "https://ip.nf/me.json", IpGetter: GetIpInJsonIpIp},
+        {Uri: "http://ip-api.com/json", IpGetter: GetIpInJsonQuery},
+        {Uri: "https://wtfismyip.com/json", IpGetter: GetIpInJsonYourFuck},
+        {Uri: "https://api.ipify.org", IpGetter: GetIpInPlainText},
+        {Uri: "https://ip.seeip.org", IpGetter: GetIpInPlainText},
+        {Uri: "https://ifconfig.me/ip", IpGetter: GetIpInPlainText},
+        {Uri: "https://ifconfig.co/ip", IpGetter: GetIpInPlainText},
     }
+    log.SetFlags(log.LstdFlags | log.Lshortfile)
     log.Printf("pid= %v", os.Getpid())
     ctx := new(ParallelHttpCtx)
     ctx.Results = list.New()
-    ctx.ResultCh = make(chan string, len(pubSrvs))
+    ctx.ResultCh = make(chan *WorkItem, len(pubSrvs))
     ctx.AllResultDoneCh = make(chan struct{})
     var cancel context.CancelFunc
     ctx.WaitCtx, cancel = context.WithCancel(context.Background())
 
     for i := 0; i < len(pubSrvs); i += 1 {
         ctx.Wg.Add(1)
-        go fetchIpRoutine(pubSrvs[i], ctx)
+        go fetchIpRoutine(&pubSrvs[i], ctx)
     }
 
     ctx.Wg.Add(1)
     go waitAllResultRoutine(ctx, len(pubSrvs))
     // wait timeout or all done
     select {
-    case <-time.After(time.Second * 2):
-        log.Printf("main timeup cancel it beforehand")
+    case <-time.After(time.Second * 3):
+        log.Printf("main timeup, cancel it beforehand")
         cancel()
     case <-ctx.AllResultDoneCh:
     }
     //log.Printf("main wait sub routine")
     ctx.Wg.Wait()
     //
-    log.Printf("fetch result cnt=%v from %v", ctx.Results.Len(), len(pubSrvs))
+    log.Printf("fetch result cnt= %v from %v", ctx.Results.Len(), len(pubSrvs))
     //fmt.Printf("The pub ip= %v\n", getTop(ctx))
+    for i := 0; i < len(pubSrvs); i += 1 {
+        log.Printf("%v -> %v", pubSrvs[i].Uri, pubSrvs[i].Result)
+    }
     fmt.Printf("%v", getTop(ctx))
     log.Printf("main exit")
 }
