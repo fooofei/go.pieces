a2d5def22134d5dded98d6e76e017869fa373b5f
fooofei <aihujianfei@gmail.com>
2019-08-04 22:52:57 +0800

    rm

--- echo_server/rlimit/rlimit_windows.go
+++
@@ -1,8 +0,0 @@
-// +build windows
-
-package rlimt
-
-// BrkOpenFilesLimit will act an empty func on Windows.
-func BrkOpenFilesLimit()  {
-    // log.Printf("called Windows version of BrkOpenFilesLimit")
-}
