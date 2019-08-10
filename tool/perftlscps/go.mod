289bcf8a943ac4497473af3244fb03dfdfe06b69
fooofei <aihujianfei@gmail.com>
2019-08-10 19:44:47 +0800

    add perftlscps

---
+++ tool/perftlscps/go.mod
@@ -0,0 +1,9 @@
+module perftlscps
+
+go 1.12
+
+require (
+	github.com/bifurcation/mint v0.0.0-20190129141059-83ba9bc2ead9
+	github.com/pkg/errors v0.8.1
+	golang.org/x/crypto v0.0.0-20190701094942-4def268fd1a4 // indirect
+)
---
+++ tool/perftlscps/tlscps-mint.go
@@ -0,0 +1,42 @@
+package main
+
+// failed for test TLSv1.3
+
+// 以后不用第三方 package 了，官方的能支持 TLSv1.3 了
+
+import (
+	"bytes"
+	"encoding/binary"
+	"encoding/hex"
+	"fmt"
+
+	"github.com/bifurcation/mint"
+)
+
+func mintHexView(v interface{}) string {
+	var binBuf bytes.Buffer
+	err := binary.Write(&binBuf, binary.LittleEndian, v)
+	if err != nil {
+		panic(err)
+	}
+
+	//return hex.EncodeToString(binBuf.Bytes())
+	return hex.Dump(binBuf.Bytes())
+}
+
+func main1() {
+
+	var raddr string
+
+	raddr = "127.0.0.1:886"
+
+	fmt.Printf("using TLSv1.3 to tcp %v\n", raddr)
+	conn, err := mint.Dial("tcp", raddr, nil)
+
+	if err != nil {
+		fmt.Printf("err = %v conn=%v \n", err, conn)
+	}
+	_, _ = conn, err
+
+	fmt.Printf("exit\n")
+}
