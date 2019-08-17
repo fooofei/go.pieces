58f5640210db7c5b91638f99ba1a93275f3ac1c0
fooofei <aihujianfei@gmail.com>
2019-08-17 21:11:00 +0800

    Update client.go

--- tool/sshttp/cmd/client/client.go
+++ tool/sshttp/cmd/client/client.go
@@ -1,13 +1,17 @@
 package main
 
 import (
+	"bytes"
 	"context"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net"
+	"net/http"
 	"os"
+	"sync"
+	"time"
 
-	"github.com/fooofei/sshttp"
 	"github.com/gorilla/websocket"
 )
 
@@ -26,7 +30,7 @@ Example:
 	return fmt.Sprintf(fmt1, program, program)
 }
 
-func main() {
+func proxy() {
 	pro := os.Args[0]
 	fmt.Printf("%v", usage(pro))
 
@@ -37,30 +41,83 @@ func main() {
 
 	addr := net.JoinHostPort(host, port)
 
-	waitCtx, cancel := context.WithCancel(context.Background())
-	d := net.Dialer{}
-	conn, err := d.DialContext(waitCtx, "tcp", addr)
+	_ = addr
+	_ = sshdPort
+	_ = sshdHost
+	// first login
+	//_, _ = conn.Write(sshttp.NewLogin())
+	//
+	//_, _ = conn.Write(sshttp.NewProxyConnect(net.JoinHostPort(sshdHost, sshdPort)))
+
+}
+
+func main() {
+	var waitCtx context.Context
+	var cancel context.CancelFunc
+
+	log.SetFlags(log.Lshortfile)
+	waitCtx, cancel = context.WithCancel(context.Background())
+	d := &websocket.Dialer{}
+
+	reqHeader := http.Header{}
+	reqHeader.Add("User-Agent", "Chrome76")
+	addr := "ws://:3389/aa"
+
+	conn, resp, err := d.DialContext(waitCtx, addr, reqHeader)
 	if err != nil {
-		log.Fatalf("dial err= %v", err)
+		log.Printf("resp= %v", resp)
+		if resp != nil {
+			log.Printf("code= %v ", resp.Status)
+			if body, err := ioutil.ReadAll(resp.Body); err == nil {
+				log.Printf("body = %s", body)
+			}
+		}
+		log.Fatalf("err= %v conn= %v", err, conn)
 	}
-	websocket.Dialer{
-		NetDial:           nil,
-		NetDialContext:    nil,
-		Proxy:             nil,
-		TLSClientConfig:   nil,
-		HandshakeTimeout:  0,
-		ReadBufferSize:    0,
-		WriteBufferSize:   0,
-		WriteBufferPool:   nil,
-		Subprotocols:      nil,
-		EnableCompression: false,
-		Jar:               nil,
+
+	log.Printf("go to loop")
+	waitGrp := &sync.WaitGroup{}
+	once := &sync.Once{}
+	connCloseFunc := func() {
+		_ = conn.Close()
+	}
+	waitGrp.Add(1)
+	go func() {
+	readLoop:
+		for {
+			msgType, msg, err := conn.ReadMessage()
+			if err != nil {
+				break readLoop
+			}
+			log.Printf("type= %v msg= len= %v \"%s\"", msgType, len(msg), msg)
+		}
+
+		waitGrp.Done()
+		once.Do(connCloseFunc)
 	}()
 
-	// first login
-	_, _ = conn.Write(sshttp.NewLogin())
+	waitGrp.Add(1)
+	go func() {
+		var cnt int64
+		var err error
+	writeLoop:
+		for {
+			select {
+			case <-time.After(time.Second * 5):
+				w := &bytes.Buffer{}
+				_, _ = fmt.Fprintf(w, "hello %v", cnt)
+				err = conn.WriteMessage(websocket.BinaryMessage, w.Bytes())
+				log.Printf("write \"%s\" err= %v", w.String(), err)
+				if err != nil {
+					break writeLoop
+				}
+			}
 
-	_, _ = conn.Write(sshttp.NewProxyConnect(net.JoinHostPort(sshdHost, sshdPort)))
+		}
+		waitGrp.Done()
+	}()
 
+	waitGrp.Wait()
+	log.Printf("main exit")
 	_ = cancel
 }
