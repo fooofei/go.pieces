57fd56e30b1fe99607c060bc78fd47c93350b1fa
fooofei <aihujianfei@gmail.com>
2019-08-25 21:11:53 +0800

    + files

---
+++ leetcode-cn/add_number_list_test.go
@@ -0,0 +1,66 @@
+package leetcode
+
+import (
+	"container/list"
+	"testing"
+)
+
+// https://leetcode-cn.com/problems/add-two-numbers/
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+
+	dl := list.New()
+	carry := 0
+	for ; l1 != nil && l2 != nil; {
+		v := l1.Val + l2.Val + carry
+		carry = v / 10
+		dl.PushBack(v % 10)
+		l1 = l1.Next
+		l2 = l2.Next
+	}
+	for ; l1 != nil; l1 = l1.Next {
+		v := l1.Val + carry
+		carry = v / 10
+		dl.PushBack(v % 10)
+	}
+	for ; l2 != nil; l2 = l2.Next {
+		v := l2.Val + carry
+		carry = v / 10
+		dl.PushBack(v % 10)
+
+	}
+	if carry > 0 {
+		dl.PushBack(carry)
+	}
+
+	var head *ListNode
+	var tail *ListNode
+	for dl.Front() != nil {
+		if tail == nil {
+			head = &ListNode{}
+			tail = head
+		} else {
+			tail.Next = &ListNode{}
+			tail = tail.Next
+		}
+		tail.Val = dl.Front().Value.(int)
+		dl.Remove(dl.Front())
+	}
+	return head
+}
+
+func TestAddTwoNumers(t *testing.T) {
+
+	l1 := &ListNode{Val: 1}
+	l1.Next = &ListNode{Val: 8}
+	l2 := &ListNode{Val: 0}
+
+	l3 := addTwoNumbers(l1, l2)
+
+	_ = l3
+}
