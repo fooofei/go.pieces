0706757b1457283faa2e7a62892d66a268d86aff
fooofei <aihujianfei@qq.com>
2020-11-06 21:45:15 +0800

    add netaddr

---
+++ pkg/netaddr/netaddr.go
@@ -0,0 +1,43 @@
+package netaddr
+
+import (
+	"bytes"
+	"encoding/binary"
+	"net"
+)
+
+func StringToNetwork(s string) (uint32, error) {
+	ip := net.ParseIP(s)
+	return binary.LittleEndian.Uint32(ip.To4()), nil
+}
+
+func NetworkToString(n uint32) (string, error) {
+	b := new(bytes.Buffer)
+	err := binary.Write(b, binary.LittleEndian, n)
+	if err != nil {
+		return "", err
+	}
+	return net.IP(b.Bytes()).String(), nil
+}
+
+func NetworkToHost(n uint32) (uint32, error) {
+	b := new(bytes.Buffer)
+	err := binary.Write(b, binary.BigEndian, n)
+	if err != nil {
+		return 0, err
+	}
+	var r uint32
+	err = binary.Read(b, binary.LittleEndian, &r)
+	return r, err
+}
+
+func HostToNetwork(h uint32) (uint32, error) {
+	b := new(bytes.Buffer)
+	err := binary.Write(b, binary.LittleEndian, h)
+	if err != nil {
+		return 0, err
+	}
+	var r uint32
+	err = binary.Read(b, binary.BigEndian, &r)
+	return r, err
+}
