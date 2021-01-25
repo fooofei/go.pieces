7816de53a57e1c79c142b67786bac36db21e79f4
fooofei <aihujianfei@qq.com>
2021-01-25 18:42:34 +0800

    rename http replay

--- test/http_replay.go
+++ test/http_replay.go
@@ -4,18 +4,38 @@ import (
 	"bufio"
 	"bytes"
 	"context"
+	"fmt"
 	"net/http"
+	"net/http/httputil"
 )
 
 // 记录如何重放 http 请求
 
+/*
 
-func template(ctx context.Context, body []byte) (*http.Request, error) {
+fastHttp
+func getFastHTTPRequest() *fasthttp.Request {
+	content, err := ioutil.ReadFile(fpath)
+	if err != nil {
+		panic(err)
+	}
+	content = bytes.TrimSpace(content)
+	req := &fasthttp.Request{}
+	err = req.Read(bufio.NewReader(bytes.NewReader(content)))
+	if err != nil {
+		panic(err)
+	}
+	return req
+}
+
+ */
+
+func requestFromString(ctx context.Context, content []byte) (*http.Request, error) {
 	var err error
-	content := bytes.TrimSpace(body)
+	var req *http.Request
 	content = append(content, []byte("\r\n\r\n")...)
 	reader := bufio.NewReader(bytes.NewReader(content))
-	req, err := http.ReadRequest(reader)
+	req, err = http.ReadRequest(reader)
 	if err != nil {
 		return nil, err
 	}
@@ -24,4 +44,23 @@ func template(ctx context.Context, body []byte) (*http.Request, error) {
 	req.URL.Scheme = "http"
 	req.URL.Host = req.Host // must set
 	return req, nil
+}
+
+func WithDumpRequest() func(*http.Request) {
+	return func(req *http.Request) {
+		// dump must before .Do()
+		content, err := httputil.DumpRequest(req, true)
+		if err == nil {
+			fmt.Printf("%s\n\n", string(content))
+		}
+	}
+}
+
+func WithDumpResponse() func(*http.Response) {
+	return func(resp *http.Response) {
+		content, err := httputil.DumpResponse(resp, true)
+		if err == nil {
+			fmt.Printf("%s\n\n---------------------------------------------\n", string(content))
+		}
+	}
 }
