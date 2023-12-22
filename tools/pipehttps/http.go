c66a2615ad4d9be7b7ad6f597ff2e4d4dd98f52a
fooofei <aihujianfei@qq.com>
2023-12-22 05:40:38 +0000

    remove unused code

--- tools/pipehttps/http.go
+++ tools/pipehttps/http.go
@@ -52,27 +52,6 @@ func WithDumpResp(w io.Writer) func(*http.Response) {
 	}
 }
 
-// CloneReqWithNewHost will clone http request from req, with updated host
-// host format is http://1.1.1.1:9090  not tail with '/'
-// 这个很重要 找了很久如何只更新连接地址，这个比较理想
-func CloneReqWithNewHost(ctx context.Context, req *http.Request, host string) (*http.Request, error) {
-	var reqNew = req.Clone(ctx)
-	if reqNew.URL.Scheme == "" {
-		reqNew.URL.Scheme = "http"
-	}
-	reqNew.URL.Host = host
-	// 不能使用 fmt.Sprintf("host:reqNew.URL.String()") 拼接结果 x.x.x.x:port/path 会导致解析报错
-	// first path segment in URL cannot contain colon
-	reqUrl, err := http.NewRequest(req.Method, reqNew.URL.String(), nil)
-	if err != nil {
-		return nil, err
-	}
-	reqNew.URL = reqUrl.URL
-	reqNew.Host = reqUrl.Host
-	reqNew.RequestURI = reqUrl.RequestURI
-	return reqNew, nil
-}
-
 // CloneReqDeep 把 request 深拷贝一份
 func CloneReqDeep(req *http.Request, ctx context.Context) *http.Request {
 	var reqNew = req.Clone(ctx)
@@ -99,14 +78,10 @@ func WithCustomDomain(m map[string]string) func(transport *http.Transport) {
 	var dialer = &net.Dialer{}
 	return func(transport *http.Transport) {
 		transport.DialContext = func(ctx context.Context, network, addr string) (net.Conn, error) {
+			// addr is must with port format
 			var addr2 = addr
 			if host, port, err := net.SplitHostPort(addr); err == nil {
-				if to, ok := m[host]; ok {
-					addr2 = net.JoinHostPort(to, port)
-				}
-			}
-			if to, ok := m[addr]; ok {
-				addr2 = to
+				addr2 = mapToHostPort(host, port, m)
 			}
 			var conn, err = dialer.DialContext(ctx, network, addr2)
 			if err != nil {
@@ -121,3 +96,38 @@ func WithCustomDomain(m map[string]string) func(transport *http.Transport) {
 		}
 	}
 }
+
+// WithCustomDomain2 跟 WithCustomDomain 一样，是另一种用法
+func WithCustomDomain2(m map[string]string) func(*http.Request) {
+	return func(req *http.Request) {
+		var defaultPort = "80"
+		if strings.EqualFold(req.URL.Scheme, "https") {
+			defaultPort = "443"
+		}
+		var srcHost = req.URL.Host
+		// 与上面的函数的区别是，这里的可能没有端口
+		if host, port, err := net.SplitHostPort(srcHost); err != nil {
+			req.URL.Host = mapToHostPort(host, defaultPort, m)
+		} else {
+			req.URL.Host = mapToHostPort(host, port, m)
+		}
+		// req.Host 决定了 http 头部是什么
+		// req.URL.Host 决定了 tcp 连接建立到哪里
+	}
+}
+
+// mapToHostPort 把 host port 根据 m 映射到另外的  hostPort
+// 优先映射端口，同时有 "a.com" -> "1.1.1.1" "a.com:443" -> "1.1.1.1:80"  map a.com:443 will result to 1.1.1.1:80
+func mapToHostPort(host, port string, m map[string]string) string {
+	var hostPort = net.JoinHostPort(host, port)
+	if portHost, ok := m[hostPort]; ok {
+		return portHost
+	}
+
+	if noPortHost, ok := m[host]; ok {
+		return net.JoinHostPort(noPortHost, port) // 只是更换 ip， 不更换端口
+	}
+	// default with src
+	return net.JoinHostPort(host, port)
+}
+
